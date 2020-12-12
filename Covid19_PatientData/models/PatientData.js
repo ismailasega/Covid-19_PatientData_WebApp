2801674494db68f9802a340cb2f42779b7c7f511
ismailasega <asega03@gmail.com>
2020-12-12 10:52:31 +0300

    Capturing Patient Data

---
+++ Covid19_PatientData/models/PatientData.js
@@ -0,0 +1,16 @@
+const { builtinModules } = require('module');
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+
+    surname:String,
+    givenName:String,
+    dob:String,
+    residence:String,
+    occupation:String,
+    nationality:String,
+    gender:String,
+    category:String
+});
+
+module.exports = mongoose.model('PatientData', patientSchema)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Covid-19_PatientData_WebApp
 Express | MongoDB |  JWTs Authentication and Authorization
+
+Information system capturing patient data

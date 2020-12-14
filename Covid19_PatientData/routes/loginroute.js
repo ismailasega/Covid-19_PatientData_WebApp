588981b5e110da9f61d500068ad02fb90abe7865
ismailasega <asega03@gmail.com>
2020-12-15 02:23:05 +0300

    Login Authentication

--- Covid19_PatientData/routes/loginroute.js
+++ Covid19_PatientData/routes/loginroute.js
@@ -1,35 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-var bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 //Declaring users
-const Users = [require('../models/Users')];
+const Users = require('../models/Users');
 
 //Tokens
 const accessTokenSecret = 'myaccess-secret';
 const refreshTokenSecret = 'myrefresh-secret';
 const refreshTokens = [];
 
+//Getting Login in page
 router.get('/login', (req, res)=>{
     res.render('login_page', {status: req.flash('status'), status2: req.flash('status2')});
 });
 
-router.post('/login', (req, res) => {
-    // read username and password from request body
-    const {username, password} = req.body;
+//Authenticating login
+router.post('/login', async(req, res) => {
+    try{
+        const userName = req.body.username;
+        const passWord = req.body.password;
 
-    // filter user from the users array by username and password
-    const user = Users.find(u => { return u.username == username && u.password == password});
-    if (user) {
-        // generate an access token
-        const accessToken  = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
-        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
-
-        refreshTokens.push(refreshToken);
-        res.redirect('/patientData');
-    
-    }else{
+        // filter user from the users collection by username
+        const user = await Users.findOne({username: userName});
+        const isMatch = bcrypt.compareSync(passWord, user.password);
+        if (isMatch) {
+            // generate an access token
+            const accessToken  = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+            const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+            refreshTokens.push(refreshToken);
+            res.redirect('/patientData');
+        }
+    }catch(err){
         req.flash('status2', 'User Credentials are Invalid');
         res.redirect('/login');
     }
@@ -59,6 +62,7 @@ router.post('/token', (req, res) => {
     });
 });
 
+//logout
 router.get('/logout', (req, res) => {
     const { token } = req.body;
     refreshTokens = refreshTokens.filter(token => t !== token);
